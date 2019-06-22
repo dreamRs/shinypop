5b282f0163f217ff2bccd455aca901b69da38c15
pvictor <perrier.victor@gmail.com>
2019-06-22 19:32:49 +0200

    examples

--- R/notie.R
+++ R/notie.R
@@ -17,7 +17,45 @@
 #' @examples
 #' if (interactive()) {
 #'
+#'   library(shiny)
 #'
+#'   ui <- fluidPage(
+#'     tags$h2("notie example"),
+#'     use_notie(),
+#'     splitLayout(
+#'       radioButtons(
+#'         inputId = "type",
+#'         label = "Type:",
+#'         choices = c("info", "neutral",
+#'                     "success", "error",
+#'                     "warning")
+#'       ),
+#'       radioButtons(
+#'         inputId = "position",
+#'         label = "Position:",
+#'         choices = c("bottom", "top")
+#'       )
+#'     ),
+#'     actionButton(
+#'       inputId = "show",
+#'       label = "Show notification",
+#'       width = "100%"
+#'     )
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'
+#'     observeEvent(input$show, {
+#'       notie(
+#'         text = "Hello world!",
+#'         type = input$type,
+#'         position = input$position
+#'       )
+#'     })
+#'
+#'   }
+#'
+#'   shinyApp(ui, server)
 #'
 #' }
 notie <- function(text,
